58422c314c014d960cd9eebba36638f5841f472a
Sara Tibbetts <sxt4866@rit.edu>
2018-04-05 00:33:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/' do
+    erb :"form.html"
+  end
+
+  put '/article' do
+    binding.pry
+  redirect to("/")
+  end
 end
